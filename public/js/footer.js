b3cb88424f9764a436695ff17279a13bafbb3ec8
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2023-07-16 20:19:26 +0700

    first commit

---
+++ public/js/footer.js
@@ -0,0 +1,61 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="img/light-logo.png" class="logo" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">men</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatershirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">women</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatershirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+        </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque impedit possimus omnis,
+    placeat delectus laborum esse veniam excepturi accusamus, praesentium totam numquam expedita. Optio eligendi 
+    culpa voluptatibus, sed voluptates quisquam iste minima, corrupti tenetur architecto quis fugit est. Aperiam adipisci iste officiis? 
+    Error totam voluptatibus vitae unde dolorem! Adipisci animi quia reiciendis optio odit tempora reprehenderit nulla! Quis pariatur eum ipsum culpa, 
+    magni aliquam eaque quia sapiente error repellendus neque magnam totam mollitia odio sequi asperiores expedita, 
+    consequuntur ad quae laborum! Ratione sunt, 
+    labore dignissimos numquam accusamus ad? Labore, quam sunt distinctio quas error molestias neque asperiores tempora nam nihil.</p>
+    <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="info">telephone - +628 232 276 6908, +628 123 445 7802</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+}
+
+createFooter();
